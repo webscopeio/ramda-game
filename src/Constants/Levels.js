3dd3f730a8195f3aee9881553f1ad9e0debb35be
Oliver Rýdzi <orydzi@gmail.com>
2017-08-30 09:22:09 +0200

    A lot of new changes, mainly solution is now JSX element and added 2 new R.compose levels.

--- src/Constants/Levels.js
+++ src/Constants/Levels.js
@@ -20,7 +20,7 @@ const levels ={
       For example on this basket {prettify({})} you can invoke <i>R.assoc('melons', true, basket)</i>, to get
       {prettify({melons: true})}
     </span>,
-    solution: "R.assoc('apples', true)",
+    solution: <span>R.assoc('apples', true)</span>,
     getUserInput: (complete) =>
       <div>
         R.{complete}(basket)
@@ -37,7 +37,7 @@ const levels ={
       "pineapples": true,
       "pears": true
     },
-    solution: "R.assoc('pears', true)",
+    solution: <span>R.assoc('pears', true)</span>,
     getUserInput: (complete) =>
       <div>
         R.{complete}(basket)
@@ -72,7 +72,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.assocPath(['apples', 'red'], true)",
+    solution: <span>R.assocPath(['apples', 'red'], true)"</span>,
   },
   3: {
     "title": "Set green apples to 'false' apples basket",
@@ -96,7 +96,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.assocPath(['apples', 'green'], false)",
+    solution: <span>R.assocPath(['apples', 'green'], false)</span>,
   },
   4: {
     "title": "Remove apples from basket",
@@ -133,7 +133,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.dissoc('pears')",
+    solution: <span>R.dissoc('pears')</span>,
   },
   6: {
     "title": "Remove red apples from the basket",
@@ -174,7 +174,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.dissocPath(['apples', 'red'])",
+    solution: <span>R.dissocPath(['apples', 'red'])</span>,
   },
   7: {
     "title": "Remove green apples from the basket",
@@ -201,7 +201,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.dissocPath(['apples', 'color', 'green'])",
+    solution: <span>R.dissocPath(['apples', 'color', 'green'])</span>,
   },
   8: {
     "title": "Pick just apples from the basket",
@@ -227,7 +227,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.pick(['apples'])",
+    solution: <span>R.pick(['apples'])</span>,
   },
   9: {
     "title": "Pick apples and pears from the basket",
@@ -244,7 +244,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.pick(['apples', 'pears'])",
+    solution: <span>R.pick(['apples', 'pears'])</span>,
   },
   10: {
     "title": "Throw away pineapples from the basket",
@@ -271,7 +271,7 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.omit(['pineapples'])",
+    solution: <span>R.omit(['pineapples'])</span>,
   },
   11: {
     "title": "Throw away apples and pears from the basket",
@@ -287,10 +287,10 @@ const levels ={
       <div>
         R.{complete}(basket)
       </div>,
-    solution: "R.omit(['apples', 'pears'])",
+    solution: <span>R.omit(['apples', 'pears'])</span>,
   },
-  "100": {
-    "title": "Learn to assoc",
+  "12": {
+    "title": "Remove pears from the basket and changed apples to true",
     "levelAssignment": {
       "apples": false,
       "pineapples": true,
@@ -299,7 +299,6 @@ const levels ={
     "resultOfLevel": {
       "apples": true,
       "pineapples": true,
-      "pears": false
     },
     getUserInput: (complete, complete2) =>
       <div>
@@ -314,12 +313,83 @@ const levels ={
             )(basket);
           </span>
       </div>,
-    "solution": [
-      "Try to use assoc to throw some apples into the basket",
-      "Try using R.assoc('apples',___)"
-    ]
+    levelHeader: 'Composing of the functions - ',
+    guide: <span>
+      In Ramda.js, you can use R.compose(<i>fn1</i>, <i>fn2</i>, <i>fn3</i>...)(object) to make number of transformations
+      on an object. Compose performs <i>right</i> to <i>left</i> composition of functions, opposed to R.pipe, that performs
+      <i>left</i> to <i>right</i> composition.
+      <br /><br />
+      For example on this basket {prettify({
+      "apples": true,
+      "pineapples": true,
+      "pears": false})}<br />
+      you can invoke <br /><br />
+        <i>R.compose(<br />
+        <div className='margin-left__30'>
+          R.omit(['pineapples', 'pears']), <br />
+          R.assoc('melons', true)<br />
+        </div>
+        )(basket)</i><br /><br /> to get
+      {prettify({apples: true, melons: true})}
+      <br />
+      <i>Note - even though we are talking about objects, compose can be used even on simple data types and arrays.</i>
+    </span>,
+    solution: <span>R.dissoc('pears') with R.assoc('apples', true)
+      <div className='color__green display-flex'>
+        or
+      </div>
+      R.omit(['pears']) with R.assoc('apples', true)
+      <div className='color__green display-flex'>
+        or
+      </div>
+      R.pick(['apples', 'pineapples']) with R.assoc('apples', true)
+    </span>,
   },
-  // "101": {
+  "13": {
+    "title": "Select just apples, olives and strawberries. Make sure they are also true.",
+    "levelAssignment": {
+      "apples": true,
+      "pineapples": true,
+      "pears": false,
+      "melons": false,
+      "olives": false,
+      "cherries": false,
+      "cucumbers": false,
+      "strawberries": true,
+      "peaches": false,
+      "plums": false
+    },
+    "resultOfLevel": {
+      "apples": true,
+      "olives": true,
+      "strawberries": true
+    },
+    getUserInput: (complete, complete2) =>
+      <div>
+          <span>
+            R.compose(
+            <br />
+            <div className='margin-left__30'>
+              R.{complete}, <br />
+              R.{complete2}
+            </div>
+            <br />
+            )(basket);
+          </span>
+      </div>,
+    solution: <span>R.pick(['apples', 'olives', 'strawberries']) with R.assoc('olives', true)
+      <div className='color__green display-flex'>
+        or
+      </div>
+      R.assoc('olives', true) with R.pick(['apples', 'strawberries']). Since <i>R.compose</i> performs right to left
+      composition, you have to be sure that R.pick is second argument of R.compose!
+      <div className='color__green display-flex'>
+        or
+      </div>
+      You can also omit each property except apples, olives and strawberries.
+    </span>,
+  },
+  // "102": {
   //   "title": "Learn to assoc",
   //   "levelAssignment": {
   //     "apples": false,
